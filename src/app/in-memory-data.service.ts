d68a1901ea0342d5d0032226cf2df8dd5cc0518e
cythiaY <cythia.xy@gmail.com>
2017-09-29 18:29:10 +0800

    词典库更新

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,15 +2,15 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         const words = [
-            { id: 1, english: 'gorgeous', explain: '极好的，称心的;', type: 'a' },
+            { id: 1, english: 'gorgeous', explain: '极好的，称心的', type: 'a' },
             { id: 2, english: 'cherish', explain: '珍惜', type: 'v' },
-            { id: 3, english: 'gorgeous', explain: '极好的，称心的;', type: 'a' },
+            { id: 3, english: 'bizarre', explain: '奇怪的，离奇的', type: 'a' },
             { id: 4, english: 'cherish', explain: '珍惜', type: 'v' },
-            { id: 5, english: 'gorgeous', explain: '极好的，称心的;', type: 'a' },
+            { id: 5, english: 'gorgeous', explain: '极好的，称心的', type: 'a' },
             { id: 6, english: 'cherish', explain: '珍惜', type: 'v' },
-            { id: 7, english: 'gorgeous', explain: '极好的，称心的;', type: 'a' },
+            { id: 7, english: 'gorgeous', explain: '极好的，称心的', type: 'a' },
             { id: 8, english: 'cherish', explain: '珍惜', type: 'v' },
-            { id: 9, english: 'gorgeous', explain: '极好的，称心的;', type: 'a' },
+            { id: 9, english: 'gorgeous', explain: '极好的，称心的', type: 'a' },
             { id: 10, english: 'cherish', explain: '珍惜', type: 'v' },
             { id: 11, english: 'icecream', explain: '冰淇淋', type: 'n' }
         ];
